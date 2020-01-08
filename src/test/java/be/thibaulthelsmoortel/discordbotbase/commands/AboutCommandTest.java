e8c61217e29998de70c63210774e895fa381a735
Thibault Helsmoortel <thibault.helsmoortel@kbc.be>
2020-01-08 10:21:22 +0100

    Remove copyrights and unused imports

--- src/test/java/be/thibaulthelsmoortel/discordbotbase/commands/AboutCommandTest.java
+++ src/test/java/be/thibaulthelsmoortel/discordbotbase/commands/AboutCommandTest.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (c) 2019 Thibault Helsmoortel.
- *
- * This file is part of Discord Bot Base.
- *
- * Discord Bot Base is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Discord Bot Base is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Discord Bot Base.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
-
 package be.thibaulthelsmoortel.discordbotbase.commands;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -26,7 +6,6 @@ import static org.mockito.Mockito.when;
 
 import be.thibaulthelsmoortel.discordbotbase.config.DiscordBotEnvironment;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/be/thibaulthelsmoortel/discordbotbase/commands/HelpCommandTest.java
+++ src/test/java/be/thibaulthelsmoortel/discordbotbase/commands/HelpCommandTest.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2019 Thibault Helsmoortel.
- *
- * This file is part of Discord Bot Base.
- *
- * Discord Bot Base is free software: you can redistribute it and/or modify
- *  t under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Discord Bot Base is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Discord Bot Base.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package be.thibaulthelsmoortel.discordbotbase.commands;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -32,7 +13,6 @@ import java.util.List;
 import java.util.Objects;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
